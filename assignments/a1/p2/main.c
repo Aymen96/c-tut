56d8ecc58eaac8cf7fb0a5859e05e63720db7628
Aymen96 <aymenhachicha96@gmail.com>
2019-10-25 12:02:21 +0000

    assignment 2 done

--- assignments/a1/p2/main.c
+++ assignments/a1/p2/main.c
@@ -1,4 +1,4 @@
-#include "testlib.h"
+#include "../testlib/testlib.h"
 #include "parseint.h"
 
 int main()
@@ -6,7 +6,7 @@ int main()
     test_start("parseint.c");
 
     test_equals_int(parseDecimalChar('0'), 0, "parseDecimalChar parses '0'");
-    test_equals_int(parseInt("010"), 8, "parseInt parses octal number");
+    test_equals_int(parseInt("067"), 55, "parseInt parses octal number 55");
     test_equals_int(parseInt("10"), 10, "parseInt parses decimal number");
     test_equals_int(parseInt("?"), -1, "parseInt handles invalid input");
 
--- assignments/a1/p2/parseint.c
+++ assignments/a1/p2/parseint.c
@@ -5,8 +5,10 @@
  */
 int parseDecimalChar(char c)
 {
-    (void)c;
-
+    int i = c;
+    if(i > 47 && i < 58) {
+        return i - 48;
+    }
     return -1;
 }
 
@@ -16,7 +18,40 @@ int parseDecimalChar(char c)
  */
 int parseInt(char *string)
 {
-    (void)string;
+    // get length of string
+    int len = 0;
+    while(string[len] != '\0') {
+        len++;
+    }
 
-    return -1;
+    int i;
+    int base;
+    int res = 0;
+    // octal number recognized by a leading zero
+    if(string[0] == '0') {
+        i = 1;
+        base = 8;
+    // decimal number
+    } else {
+        i = 0;
+        base = 10;
+    }
+    while(string[i] != '\0') {
+        int el = parseDecimalChar(string[i]);
+        if(el == -1) {
+            return -1;
+        }
+        // octal numbers have digits in [0..7]
+        if(base == 8 && (string[i] == '8' || string[i] == '9')) {
+            return -1;
+        }
+        int e = 0;
+        while(e < len - 1 - i) {
+            el *= base;
+            e++;
+        }
+        res += el;
+        i++;
+    }
+    return res;
 }
