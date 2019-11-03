b794e5c087a1283732a51ca9f0b36b60217f8ab9
Aymen96 <aymenhachicha96@gmail.com>
2019-11-03 22:51:41 +0000

    implemented tasked as asked

--- assignments/a2/p2/hash.c
+++ assignments/a2/p2/hash.c
@@ -1,12 +1,18 @@
 #include "hash.h"
+#include <stdio.h>
+#include <string.h>
 
 /*
  * Returns a hash value for the given string.
  */
 uint64_t hashString(char *c)
 {
-	(void) c;
-	return 0;
+	int i;
+    uint64_t hash = 5381;
+    for (i = 0; c[i] != '\0'; i++) {
+        hash = hash * 33 + c[i];
+    }
+	return hash;
 }
 
 /*
@@ -15,6 +21,19 @@ uint64_t hashString(char *c)
 int insert(hashtable *table, char *key, int value)
 {
 	(void) table; (void) key; (void) value;
+    int pos = hashString(key) % table->capacity;
+    element el;
+    el.key = key;
+    el.value = value;
+    el.valid = 1;
+    int i = 0;
+    while((table->elements + pos + i)->valid == 1 && pos + i < (int) table->capacity) {
+        i++;
+    }
+    if (pos + i < (int) table->capacity) {
+        *(table->elements + pos + i) = el;
+        return 1;
+    }
 	return 0;
 }
 
@@ -23,7 +42,16 @@ int insert(hashtable *table, char *key, int value)
  */
 int find(hashtable *table, char *key, int *value)
 {
-	(void) table; (void) key; (void) value;
+    int i = 0;
+    while(i < (int) table->capacity) {
+        if((table->elements + i)->valid != 0) {
+            if(strcmp((table->elements + i)->key, key) == 0) {
+                *value = (table->elements + i)->value;
+                return 1;
+            }
+        }
+        i++;
+    }
 	return 0;
 }
 
