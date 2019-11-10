298405a3f1d139233f64f141b6943e70bf4e1292
Aymen96 <aymenhachicha96@gmail.com>
2019-11-10 22:58:29 +0000

    solved programming question 1 a3

--- assignments/a3/p1/insert.c
+++ assignments/a3/p1/insert.c
@@ -1,5 +1,6 @@
 #include "insert.h"
 
+#include <stdlib.h>
 #include <string.h>
 
 /*
@@ -20,5 +21,22 @@ void insert_tut(int *a, size_t *length, int z)
  */
 void insert(int **array, size_t *length, size_t *capacity, int z)
 {
-	(void) array; (void) length; (void) capacity; (void) z;
+    if(*length + 1 > *capacity) {
+        *capacity = *capacity + 10;
+        int* array2 = (int*) malloc(sizeof(int) * *capacity);
+        memcpy(array2, *array, *length);
+        /*
+        // Question: why does the following not working instead of using memcpy
+        // I copy manually the data, free array and then assign the pointer array
+        // the pointer of the new array with the bigger capacity.
+        // Would appreciate your help on this matter.
+        int i;
+        for (i = 0; i < (int) *length; i++) {
+            *(array2 + i) = (*array)[i];
+        }
+        free(*array);
+        array = &array2;
+        */
+    }
+    insert_tut(*array, length, z);
 }
